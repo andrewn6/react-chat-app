58474258babbaba2a9f5a30a027a951019a6c98a
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-14 18:46:51 -0400

    finish setup + /secured for JWT

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,7 +1,16 @@
-
-
-
+const authRoute = require('routes/auth');
+const contactRoute = require('routes/contact');
+const messageRoute = require('routes/message');
+const defaultRoute = require('routes/default');
+const conversationRoute = require('routes/conversation');
+const jwtMiddleware = require('middlewares/jwt');
 
 module.exports = (app) => {
-  
-}
+  app.use('/');
+  app.use('/secured', jwtMiddleware);
+  app.use(authRoute);
+  app.use(contactRoute);
+  app.use(messageRoute);
+	app.use(conversationRoute);
+	app.use(defaultRoute);
+};
