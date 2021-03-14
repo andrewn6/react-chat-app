93b1d34aca0f44e592cbe209841ddc53d735c2c3
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-14 11:48:48 -0400

    finish auth validator check if password is smaller, and nickanem is taken

--- server/app/validators/auth.js
+++ server/app/validators/auth.js
@@ -12,4 +12,21 @@ module.exports.signupValidator = () => [
   check('nickname')
     .isLength({ min:3, max: 14})
     .withMessage(constants.EXPRESS_VALIDATION_MESSAGES.NICKNAME_LENGHT_BETWEEN_2_AND_12)
+    .custom(nickname => Promise(async (resolve, reject) => {
+      try {
+        const user = await findUser({
+          nickname: nickname.toLowerCase()
+        });
+        if (user.length > 0) {
+          reject();
+        }
+        resolve();
+      } catch(e) {
+        reject();
+      }
+    }))
+    .withMessage(constants.EXPRESS_VALIDATION_MESSAGES.THIS_NICKNAME_IS_ALREADY_TAKEN)
+  check('password')
+    .isLength({min:5, max: 12})
+    .withMessage(constants.EXPRESS_VALIDATION_MESSAGES.PASSWORD_LENGHT_BETWEEN_5_AND_12)
 ];
