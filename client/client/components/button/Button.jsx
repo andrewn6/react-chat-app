a9e41b64ffce48e74bee25bc12d6f301b4de3d7a
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-14 20:23:03 -0400

    es6 causing issues, test button + use shared/components

---
+++ client/client/components/button/Button.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Component } from 'react'
+
+import { LoadingComponent } from 'shared/components';
+
+export default class Button extends Component {
+  render () {
+		const {
+			text,
+			primary,
+			link,
+			width,
+			onClick,
+			type,
+			isFetching,
+			disabled,
+			children,
+			margin,
+			defaultButton,
+			outline,
+			small,
+			setRef
+    } = this.props;
+    
+    const buttonStyles = classNames({
+			button: true,
+			'button--primary': primary && !disabled,
+			'button--disabled': disabled,
+			'button--link': link,
+			'button--default': defaultButton && !disabled,
+			outline,
+			small
+    });
+    
+    return(
+			<button
+				onClick={(event) => {
+					event.stopPropagation();
+					if (onClick) {
+						onClick(event);
+					}
+				}}
+				className={buttonStyles}
+				style={{
+					width,
+					margin
+				}}
+				type={type}
+				disabled={disabled}
+				ref={setRef}
+			>
+				{
+						isFetching && !disabled ? (
+							<LoadingComponent
+								type="spinner"
+							/>
+						) : text
+				}
+			</button>
+		);
+	}
+}
