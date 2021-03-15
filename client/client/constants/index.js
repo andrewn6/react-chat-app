4a0493e00e977bf7d1c72c3e683f407523e972cc
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-14 20:49:42 -0400

    seperate dirs + get cookie setremove etc.

---
+++ client/client/constants/index.js
@@ -0,0 +1,3 @@
+import devConfig from 'configs/dev.json';
+
+export default (process.env.NODE_ENV.toLowerCase() === 'development' ? devConfig
--- client/client/modules/constants/index.js
+++
@@ -1,3 +0,0 @@
-import devConfig from 'configs/dev.json';
-
-export default (process.env.NODE_ENV.toLowerCase() === 'production' ? devConfig
